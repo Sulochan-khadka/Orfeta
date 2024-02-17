46035b7e742e2fe86e6ef4679e4b85c6c0254107
sulochan <sulochankhadka25@gmail.com>
2024-02-18 02:23:24 +0530

    commit

--- src/components/RotationWrapper.tsx
+++ src/components/RotationWrapper.tsx
@@ -1,5 +1,5 @@
 import { useFrame } from '@react-three/fiber';
-import React, { useRef } from 'react';
+import { useRef } from 'react';
 import * as THREE from 'three';
 
 export default function RotationWrapper(props) {
